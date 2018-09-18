91ff0b4fc8f0ce61955e6ff8037b5f21233aae4c
John Knotts <jhnnyk@gmail.com>
2018-09-17 21:08:17 -0600

    protect post routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,12 @@ class ApplicationController < ActionController::Base
     @current_user || User.find(session[:user_id]) if session[:user_id]
   end
 
+  def require_login
+    unless logged_in?
+      flash[:danger] = 'Please log in.'
+      redirect_to home_url
+    end
+  end
+
   helper_method :logged_in?
 end
