4fe4c85f1607ef350cec3f5aac84a4e563e5d563
John Knotts <jhnnyk@gmail.com>
2018-09-19 19:15:46 -0600

    disable user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,18 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+  # Commenting this out so that no one else can sign up as a user on my blog!
+  # def new
+  #   @user = User.new
+  # end
 
-  def create
-    @user = User.new(allowed_params)
+  # def create
+  #   @user = User.new(allowed_params)
 
-    if @user.save
-      redirect_to home_path, notice: 'Thank you for signing up!'
-    else
-      render :new
-    end
-  end
+  #   if @user.save
+  #     redirect_to home_path, notice: 'Thank you for signing up!'
+  #   else
+  #     render :new
+  #   end
+  # end
 
   private
 
